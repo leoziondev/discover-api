64aec43be6f9464675d2a5b17dca68a16f3cd9b4
LeoZion <leozionsuporte@gmail.com>
2022-07-21 22:38:58 -0300

    get routes

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.route('/').get((req, res) => {
+  res.send("Home Page")
+})
+
+app.route('/about').get((req, res) => {
+  res.send("About Page")
+})
+
 app.listen('3000')
